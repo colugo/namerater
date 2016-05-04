a7e66ba791c52332e61a85eca9ad42d77606dae4
colugo <colugo.info@gmail.com>
2016-05-04 11:23:00 +1000

    Fix off-by-one issue with randomable code

--- app/models/concerns/randomable.rb
+++ app/models/concerns/randomable.rb
@@ -5,7 +5,7 @@ module Randomable
 
   class_methods do
     def random(the_count = 1)
-      records = offset(rand(count - the_count)).limit(the_count)
+      records = offset(rand(count - the_count + 1)).limit(the_count)
       the_count == 1 ? records.first : records
     end
   end
