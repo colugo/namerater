53fd9f30e2d2d4ee33b3cf1b683d80b255a6fb95
colugo <colugo.info@gmail.com>
2016-05-04 13:58:09 +1000

    Host the API from /, not /names

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :titles, except: [:new, :edit]
   resources :actions, except: [:new, :edit]
-  get "/names/*config" => "namerators#random"
+  get "/*config" => "namerators#random"
   get "/" => "pages#show"
 end
