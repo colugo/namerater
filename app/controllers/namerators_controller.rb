5d913752c6da98fedb2b6063a2fd7adecad96cd2
colugo <colugo.info@gmail.com>
2016-05-04 13:05:43 +1000

    Add validation for valid categories, and the number of categories

--- app/controllers/namerators_controller.rb
+++ app/controllers/namerators_controller.rb
@@ -7,6 +7,7 @@ class NameratorsController < ApplicationController
 
     @config = params[:config].split("/")
     getCount()
+    validateCategories() or return
 
     @namerators = []
     (1..@count).each do
@@ -15,12 +16,41 @@ class NameratorsController < ApplicationController
     render json: @namerators
   end
 
+  def validateCategories()
+    if(@config.size > 5 )
+      render json: "Maximum of 5 categories allowed", status: :unprocessable_entity and return
+    end
+    #@config.each{ |category| verifyCategory(category) or return }
+    return true
+  end
+
+  def verifyCategory(category)
+    return true if(category == "Category")
+    if Category.find_by(name: category) == nil
+      render json: "No category: #{category}", status: :unprocessable_entity and return
+    end
+    return true
+  end
+
   def getCount()
-    @count = Integer(@config.pop) rescue DEFAULT_RESULTS
-    if @count > MAX_RESULTS
-       @count = MAX_RESULTS
+    if hasCount()
+      @count = Integer(@config.pop) rescue DEFAULT_RESULTS
+      if @count > MAX_RESULTS
+         @count = MAX_RESULTS
+      end
+    else
+      @count = DEFAULT_RESULTS
     end
   end
+
+  def hasCount()
+    count = Integer(@config.last) rescue false
+    if count == false
+      return false
+    end
+    true
+  end
+
 
   # validation?
   #if count > MAX_RESULTS
